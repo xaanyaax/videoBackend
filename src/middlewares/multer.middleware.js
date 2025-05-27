cc3293489cd8cfb4684eca327811e6e7393268e9
xaanyaax <aanyasingh282004@gmail.com>
2025-05-28 01:06:18 +0530

    introduced multer

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,13 @@
+import multer from "multer";
+
+// Define storage
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/'); // Directory to save uploaded files
+    },
+    filename: (req, file, cb) => {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+      const ext = path.extname(file.originalname);
+      cb(null, file.fieldname + '-' + uniqueSuffix + ext);
+    }
+  });
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+
+
+  const uploadOnCloudinary = async(localFilePath) => {
+      try{
+          if(!localFilePath) return null;
+          //upload file on cloudinary
+          const response = awaitcloudinary.uploader.upload(localFilePath , {
+            resource_type: "auto"
+          });
+          //file has been uploaded sucessfully
+
+          console.log("file has been uploaded sucessfully" , response.url);
+          return response;
+      }
+      catch(error){
+          fs.unlinkSync(localFilePath); //delete file from local storage as the upload operation got failed
+          return null;
+      }
+  }
+
+  export {uploadOnCloudinary}
