9d82ee5179460f9154d18fd932b2287fb27155a1
xaanyaax <aanyasingh282004@gmail.com>
2025-05-29 03:09:59 +0530

    added login congig

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,53 @@
 import { asyncHandler } from "../utils/asyncHandler";
-
+import { ApiError } from "../utils/ApiError";
+import {User} from "../models/user.model.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const registerUser = asyncHandler( async (req , res) => {
-    res.status(200).json({
-        message : "ok"
+    // algorithm:>>
+    // get user deatils from frontend
+    // validation on name , email etc not empty
+    // check if user already exist : username or email
+    // check for images , check for avatar
+    // upload them to cloudinary , avatar check for multer
+    // create user object - create entry in db
+    // remove password and refresh token feild from response
+    // check for user creation
+    // return response
+
+    const {fullname , email , username , password} = req.body
+    console.log("email : " , email)
+
+    if([fullname , email , username , password].some((field) => field?.trim() === "")){   //advances coded
+        throw new ApiError(400 , "all fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or : [
+            {username},
+            {email}
+        ]
     })
-})
+
+    if(existedUser){
+        throw new ApiError(409 , "user already exist")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0].path;
+    const coverImageLocalPath = req.files?.coverImage[0].path
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400 , "avatar is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)          
+    
+    if(!avatar){
+        throw new ApiError(500 , "avatar file is required")
+    }
+
+    User
+}) 
 
 export {registerUser}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
-
+// import {upload} from "../utils/multer // if you want to use multer for file uploads
 const router = Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+        {
+        name: "avatar" ,
+        maxCount: 1
+    },
+
+    {
+        name : "coverImage",
+        maxcount : 1
+    }
+]),
+    registerUser)
 // router.route("/login").post(registerUser)
 
 export default router
